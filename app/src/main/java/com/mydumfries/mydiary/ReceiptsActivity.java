3df2c37e5ab298f2c0365dae84cc63d309eecaa3
MyDumfries <55667504+MyDumfries@users.noreply.github.com>
2020-04-29 11:38:37 +0100

    test

---
+++ app/src/main/java/com/mydumfries/mydiary/ReceiptsActivity.java
@@ -0,0 +1,49 @@
+package com.mydumfries.mydiary;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ReceiptsActivity  extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.receipts);
+        final TextView onenote = (TextView) findViewById(R.id.launchonenote);
+        onenote.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                PackageManager packageManager = getPackageManager();
+                String packageName = "com.microsoft.office.onenote";
+                try {
+                    Intent intent = packageManager
+                            .getLaunchIntentForPackage(packageName);
+                    if (null != intent) {
+                        startActivity(intent);
+                    }
+                } catch (ActivityNotFoundException e) {
+
+                }
+            }
+        });
+        final TextView lens = (TextView) findViewById(R.id.launchlens);
+        lens.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                PackageManager packageManager = getPackageManager();
+                String packageName = "com.microsoft.office.officelens";
+                try {
+                    Intent intent = packageManager
+                            .getLaunchIntentForPackage(packageName);
+                    if (null != intent) {
+                        startActivity(intent);
+                    }
+                } catch (ActivityNotFoundException e) {
+
+                }
+            }
+        });
+    }
+}
